6b30ec960d97a77121873030964e7542552a1f44
Christopher Cheechov <rushands@gmail.com>
2020-10-31 23:46:26 -0700

    last minute commit of unfinished homework

---
+++ script.js
@@ -0,0 +1,8 @@
+// variable to store current time
+var currentTime = $.now();
+console.log(currentTime)
+
+// variables to save data entered upon clicking save button
+var clickSave0 = $("#save0").on("click", function(){
+    var saveText0 = $("textarea#0").val();
+})
